4c74c2e0a66052601369eb83ff0b22c3ac9b8f6d
odelayIO <support@odelay.io>
2023-02-07 09:44:28 -0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # Bayclub-Bot
+
+I'm not proud of using my engineering skills to cheat the system, but I'm over 40 years old and need to workout to stay healthy.
+
+
+
+## System Configuration
+
+The Bayclub-Bot is designed to run on a Linux operating system to take advantage of CRON jobs and Chrome webbrowser.  One can update the script and setup to operate on Windows.  
+
+## Python Script
+
+The script uses `selenium` to automate the booking for the Ignite class at the El Segundo location.  This script is very stupid, and just clicks on the buttons that is current on the Bayclub webpage.  So if the class order changes, then the script will fail, since it assumes each day during booking will have the same number of classes on the page.  This is due to the indexing of the classes on the webpage.  Please feel free to update the script to search for the work "ignite", then click the button for booking.
+
+Install `selenium`:
+
+```bash
+pip3 install selenium
+```
+
+
+
+## Installing Chromedriver
+
+The `chromedriver` will need to be installed on the Linux system:
+
+Download the chromedriver: https://chromedriver.chromium.org/downloads
+
+Select the version for the Chrome version installed on the system.  Then you will need to copy into the `/usr/bin` folder
+
+```bash
+sudo cp chromedriver /usr/bin/.
+```
+
+
+
+## Setting up Cron Job
+
+All classes can be booked 3 days out from the day.  Therefore the `cron` job will start just before 7am, and execute the script which will set a timer to wait for 3 seconds after 7am (e.g. 7:00:03).  
+
+Execute the following command on the terminal:
+
+```bash
+crontab -e
+```
+
+Then copy the following three lines into the crontab file:
+
+```bash
+59 6 * * 2 export DISPLAY=:0 && cd /home/sdr/workspace/bayclub-bot && /usr/bin/python3 bayclub-bot_Book_Ignite_Class.py > bayclub-cron.log 2>&1
+59 6 * * 5 export DISPLAY=:0 && cd /home/sdr/workspace/bayclub-bot && /usr/bin/python3 bayclub-bot_Book_Ignite_Class.py > bayclub-cron.log 2>&1
+59 6 * * 7 export DISPLAY=:0 && cd /home/sdr/workspace/bayclub-bot && /usr/bin/python3 bayclub-bot_Book_Ignite_Class.py > bayclub-cron.log 2>&1
+
+```
+
+
+
+## Configure `bayclub-bot_Book_Ignite_Class.py` Bot
+
+The following parameters will need to be udpated with the user name and password in the script.  Yeah, I said this script is stupid. Update `_USER_NAME` and `_USER_PASS` with the login for Bayclub.
+
+```python
+_USER_NAME      = 'your-user-name'
+_USER_PASS      = 'your-password'
+```
+
---
+++ bayclub-bot_Book_Ignite_Class.py
@@ -0,0 +1,167 @@
+
+
+#############################################################################################
+#############################################################################################
+#
+#   The MIT License (MIT)
+#   
+#   Copyright (c) 2021 http://odelay.io 
+#   
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#   
+#   The above copyright notice and this permission notice shall be included in all
+#   copies or substantial portions of the Software.
+#   
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+#   
+#   Contact : <everett@odelay.io>
+#  
+#   Description : Build script for GNURadio with RFNoC
+#
+#   Version History:
+#   
+#       Date        Description
+#     -----------   -----------------------------------------------------------------------
+#      07FEB2023     Original Creation
+#
+###########################################################################################
+#
+# Execute script to book Ignite Class, see README.md for instructions.
+#
+###########################################################################################
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import datetime
+
+_USER_NAME      = 'your-user-name'
+_USER_PASS      = 'your-password'
+
+_DELAY          = 0.00
+_ENABLE_TIMER   = True
+_CLASS_TIME_HR  = 7  # Hours in 24hr Time
+_CLASS_TIME_MIN = 0  # Minutes in 24hr Time
+
+_CLASS_TIME     = _CLASS_TIME_HR*100+_CLASS_TIME_MIN
+
+
+# -----------------------------------------------------------
+#   Setting timer to sleep for exact seconds
+# -----------------------------------------------------------
+if(_ENABLE_TIMER):
+    t = datetime.datetime.today()
+    f = datetime.datetime(t.year,t.month,t.day,_CLASS_TIME_HR,_CLASS_TIME_MIN,3)  # 7am Class
+    print("Auto Booking with Delay...")
+    print("  Current Time         : " + str(t))
+    print("  Booking Time         : " + str(f))
+    s = (f-t).total_seconds()
+    print("  Seconds to Booking   : " + str(s))
+    
+    print("Sleeping for " + str(s) + "seconds...")
+    time.sleep(s)
+else:
+    print("Auto Booking Now...")
+
+
+# -----------------------------------------------------------
+#   Open Chrome and go to website
+# -----------------------------------------------------------
+browser = webdriver.Chrome()
+#browser.get(('https://bayclubconnect.com/home/member-dashboard'))
+browser.get(('https://bayclubconnect.com/classes'))
+
+wait = WebDriverWait(browser,10)
+print("Opening Chrome...")
+
+
+# -----------------------------------------------------------
+#   Log into system
+# -----------------------------------------------------------
+# Enter User Name    
+username = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='username']")))
+username.send_keys(_USER_NAME)
+
+
+# Enter User Password
+password = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='password']")))
+password.send_keys(_USER_PASS)
+
+# Click Login button
+login_button = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/app-login/div/app-login-connect/div[1]/div/div/div/form/button")))
+login_button.click()
+time.sleep(_DELAY)
+print("Logged into Bayclub...")
+
+#           # -----------------------------------------------------------
+#           #   Select Location
+#           # -----------------------------------------------------------
+#           plan_a_visit = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/app-navbar/nav/div/div/button")))
+#           plan_a_visit.click()
+#           time.sleep(_DELAY)
+#           
+#           
+#           el_segundo_location = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/modal-container/div/div/app-schedule-visit-desktop/div/div/app-schedule-visit-club/div/div[1]/div/div[2]/div/div[3]/div[1]/div/app-radio-select/div/div[2]/div/span[1]")))
+#           el_segundo_location.click()
+#           time.sleep(_DELAY)
+#           
+#           
+#           club_location_continue_button = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/modal-container/div/div/app-schedule-visit-desktop/div/div/app-schedule-visit-club/div/div[2]/div/div")))
+#           club_location_continue_button.click()
+#           time.sleep(_DELAY)
+#           
+#           
+#           # -----------------------------------------------------------
+#           #   Select Fitnes 
+#           # -----------------------------------------------------------
+#           fitness = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/modal-container/div/div/app-schedule-visit-desktop/div/div/app-schedule-visit-service/div/div/div/div[4]/div[2]/div/div")))
+#           fitness.click()
+#           time.sleep(_DELAY)
+
+# -----------------------------------------------------------
+#   Select Day to book (always 3 days from today)
+# -----------------------------------------------------------
+class_day = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/app-classes-shell/app-classes/div/div[1]/div/app-classes-filters/div/form/div[4]/div/app-date-slider/div/div[2]/gallery/gallery-core/div/gallery-slider/div/div/gallery-item[1]/div/div/div[4]/div[1]")))
+class_day.click()
+time.sleep(_DELAY)
+print("Selected Day...")
+
+
+# -----------------------------------------------------------
+#   Book Ignite and confirm
+# -----------------------------------------------------------
+# 7am Class
+if(_CLASS_TIME == 700):
+    book_ignite = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/app-classes-shell/app-classes/div/app-classes-list/div/div[3]/app-classes-can-book-item/app-class-list-item/div/div[1]/div[5]/div")))
+
+# 8:30am Class
+if(_CLASS_TIME == 830):
+    book_ignite = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/app-root/div/app-classes-shell/app-classes/div/app-classes-list/div/div[7]/app-classes-can-book-item/app-class-list-item/div/div[1]/div[5]/div")))
+book_ignite.click()
+time.sleep(_DELAY)
+
+confirm = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/modal-container/div/div/app-universal-confirmation-modal/div[2]/div/div/div[4]/div/button[1]")))
+confirm.click()
+time.sleep(_DELAY)
+
+# -----------------------------------------------------------
+#   Close Chrome
+# -----------------------------------------------------------
+time.sleep(10)
+browser.close()
+print("Booked Ignite Class")
+
+
